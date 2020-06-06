edeaf6e78fd9b2d8c4dc12d3c2338b83abd6ff7f
Mikhail Matsehora <mishamacegora3@gmail.com>
2020-06-06 17:52:54 +0300

    Completed the first lesson

---
+++ script.js
@@ -0,0 +1,9 @@
+let money = 'Привет мир!';
+let income = 1000000000;
+let addExpenses = 200000;
+let deposit = 100000;
+let mission = 'Я хочу выучить JS';
+let period = 4;
+
+alert(money);
+console.log(period);

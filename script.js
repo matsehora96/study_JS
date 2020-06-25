d3088d2ad0655abfc3d48a94653a487a69d69372
Mykhailo Matsehora <m.matsehora@pin-up.team>
2020-06-25 10:18:03 +0300

    finish lesson14

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
 'use strict';
 
-function DomElement (selector, height, width, bg, fontSize) {
+function DomElement (selector, height, width, bg, fontSize, inputText) {
     this.selector = selector;
     this.height = height;
     this.width = width;
     this.bg = bg;
     this.fontSize = fontSize;
+    this.inputText = inputText;
 }
 
 DomElement.prototype.createElement = function() {
-    let newDiv = document.querySelector("body");
+    let newDiv = document.createElement("div");
     if (this.selector.substr(0, 1) === '.') {
-        newDiv.innerHTML = "<div class='element'></div>" ; 
+        newDiv.className = this.selector.substr(1);
+        newDiv.style.height = this.height + "px";
+        newDiv.style.width = this.width + "px";
+        newDiv.style.background = this.bg;
+        newDiv.style.fontSize = this.fontSize + "px";
+        newDiv.innerHTML = this.inputText;
+        document.body.appendChild(newDiv);
     } else if (this.selector.substr(0, 1) === '#') {
-        newDiv.innerHTML = "<div id='element'></div>" ; 
+        newDiv.id = this.selector.substr(1);
+        newDiv.style.height = this.height + "px";
+        newDiv.style.width = this.width + "px";
+        newDiv.style.background = this.bg;
+        newDiv.style.fontSize = this.fontSize + "px";
+        newDiv.innerHTML = this.inputText;
+        document.body.appendChild(newDiv);
     }
-    console.log(newDiv);
 };
 
-const appData = new DomElement('#div' , '30' , '30', 'black', '20');
-console.log(appData);
-appData.createElement();
+const elemFirst = new DomElement('#element' , '50' , '400', 'green', '20', 'Привет, я Глаша');
+const elemSecond = new DomElement('.element' , '100' , '500', 'red', '40', 'Привет, я Миша');
+
+elemFirst.createElement();
+elemSecond.createElement();
